12d48316b773fa937713e3587e75fad05009a765
ssanghyuk <ssanghyuk23@gmail.com>
2020-05-30 19:13:21 +0900

    test commit

--- app/src/main/java/com/example/biki/MainActivity.java
+++ app/src/main/java/com/example/biki/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //comments is easy way to add useless commit
     }
 }
